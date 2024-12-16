57f6b6ac97073566748507f319912d36cdaffd9a
alyaaa21 <alya.dwipangesti21@gmail.com>
2024-12-16 15:59:41 +0800

    KataTurunan.java
    DefinEZ

--- KataTurunan.java
+++ KataTurunan.java
@@ -1,21 +1,27 @@
 class KataTurunan {
+    //referensi ke kamus, ntar buat nyari kata
     private Kamus kamus;
 
+    //ini konstruktornya, objek kamus yang ntar dipake buat nyari kata
     public KataTurunan(Kamus kamus) {
         this.kamus = kamus;
     }
 
+    //method buat nampilin kata turunan
     public void tampilkanTurunanKata(String kataInduk) {
+        //cari node kata induk yang dimaksud di kamus
         TreeNode nodeInduk = kamus.cariKata(kataInduk);
-        
+
+        //kalo kata induk gaada di kamus, pesan error
         if (nodeInduk == null) {
             System.out.println("Kata induk tidak ditemukan: " + kataInduk);
             return;
         }
 
+        //kalo kata induk ada di kamus
         System.out.println("Kata Turunan untuk '" + kataInduk + "':");
+        //imbuhan buat kata turunan
         String[] akhiran = {"-an", "-kan", "me-", "pe-"};
-        
         for (String akhir : akhiran) {
             String kataTurunan = generateTurunanKata(kataInduk, akhir);
             TreeNode nodeTurunan = kamus.cariKata(kataTurunan);
